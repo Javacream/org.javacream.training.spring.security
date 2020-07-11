3f04cfcda2a5afcb47c6ec77b2585d721e9f595e
Rainer Sawitzki <training@rainer-sawitzki.de>
2020-07-11 21:17:38 +0200

    add Jwt Service

--- org.javacream.training.spring.security/src/main/java/org/javacream/training/spring/security/WebSecurityConfiguration.java
+++ org.javacream.training.spring.security/src/main/java/org/javacream/training/spring/security/WebSecurityConfiguration.java
@@ -2,6 +2,7 @@ package org.javacream.training.spring.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -17,29 +18,23 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http
-			.authorizeRequests()
-				.antMatchers("/api/anonymous").permitAll()
-				.anyRequest().authenticated()
-				.and()
-			.formLogin()
-				.permitAll()
-				.and()
-			.logout()
-				.permitAll();
+		http.authorizeRequests().antMatchers("/", "/api/anonymous", "/authenticate/**", "/login*").permitAll().anyRequest().authenticated().and()
+				.formLogin().permitAll().and().logout().permitAll();
 	}
 
 	@Bean
 	@Override
 	public UserDetailsService userDetailsService() {
 		@SuppressWarnings("deprecation")
-		UserDetails user =
-			 User.withDefaultPasswordEncoder()
-				.username("user")
-				.password("password")
-				.roles("USER")
+		UserDetails user = User.withDefaultPasswordEncoder().username("user").password("password").roles("USER")
 				.build();
 
 		return new InMemoryUserDetailsManager(user);
+	}
+
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
 	}
 }
