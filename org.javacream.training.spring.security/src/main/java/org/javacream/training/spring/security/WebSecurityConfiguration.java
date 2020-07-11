37416b43dfe3750e4203b0707fe0460cdf3300bf
Rainer Sawitzki <training@rainer-sawitzki.de>
2020-07-11 20:30:44 +0200

    add simple authorization

--- org.javacream.training.spring.security/src/main/java/org/javacream/training/spring/security/WebSecurityConfiguration.java
+++ org.javacream.training.spring.security/src/main/java/org/javacream/training/spring/security/WebSecurityConfiguration.java
@@ -2,6 +2,7 @@ package org.javacream.training.spring.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,6 +13,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -35,7 +37,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 			 User.withDefaultPasswordEncoder()
 				.username("user")
 				.password("password")
-				.roles("user")
+				.roles("USER")
 				.build();
 
 		return new InMemoryUserDetailsManager(user);
