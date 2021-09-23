f22b8e5b2023e987dd901f7d83d1689f6d384fff
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-09-23 20:32:13 +0200

    add basic authentication and authorization

--- org.javacream.training.security.webservice.jar/src/main/java/org/javacream/training/security/webservice/WebSecurityConfiguration.java
+++ org.javacream.training.security.webservice.jar/src/main/java/org/javacream/training/security/webservice/WebSecurityConfiguration.java
@@ -7,7 +7,6 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity(debug = true)
@@ -27,6 +26,4 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic();    }
-
-
 }
