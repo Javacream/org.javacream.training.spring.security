599d5a49d212485459ae312c13a54e98ae9f7f7b
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-09-23 21:06:58 +0200

    implement Kerberos authentication for Spring Boot

--- org.javacream.training.security.webservice.jar/src/main/java/org/javacream/training/security/webservice/WebSecurityConfiguration.java
+++ org.javacream.training.security.webservice.jar/src/main/java/org/javacream/training/security/webservice/WebSecurityConfiguration.java
@@ -1,29 +1,108 @@
 package org.javacream.training.security.webservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.kerberos.authentication.KerberosAuthenticationProvider;
+import org.springframework.security.kerberos.authentication.KerberosServiceAuthenticationProvider;
+import org.springframework.security.kerberos.authentication.sun.SunJaasKerberosClient;
+import org.springframework.security.kerberos.authentication.sun.SunJaasKerberosTicketValidator;
+import org.springframework.security.kerberos.web.authentication.SpnegoAuthenticationProcessingFilter;
+import org.springframework.security.kerberos.web.authentication.SpnegoEntryPoint;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity(debug = true)
 @EnableGlobalMethodSecurity(securedEnabled = true)
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-	
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-          .withUser("user").password("{noop}user123")
-          .authorities("ROLE_USER").and()
-          .withUser("admin").password("{noop}admin123")
-          .authorities("ROLE_USER", "ROLE_ADMIN");
-    }
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("user").password("{noop}user123").authorities("ROLE_USER").and()
+				.withUser("admin").password("{noop}admin123").authorities("ROLE_USER", "ROLE_ADMIN");
+	}
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic();    }
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+        .addFilterBefore(
+            spnegoAuthenticationProcessingFilter(),
+            BasicAuthenticationFilter.class);
+	}
+
+	@Bean
+	public SpnegoEntryPoint spnegoEntryPoint() {
+		return new SpnegoEntryPoint("/");
+	}
+
+	@Bean
+	public SpnegoAuthenticationProcessingFilter spnegoAuthenticationProcessingFilter() {
+		SpnegoAuthenticationProcessingFilter filter = new SpnegoAuthenticationProcessingFilter();
+		try {
+			AuthenticationManager authenticationManager = authenticationManagerBean();
+			filter.setAuthenticationManager(authenticationManager);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return filter;
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.authenticationProvider(kerberosAuthenticationProvider())
+				.authenticationProvider(kerberosServiceAuthenticationProvider());
+	}
+
+	@Bean
+	public KerberosAuthenticationProvider kerberosAuthenticationProvider() {
+		KerberosAuthenticationProvider provider = new KerberosAuthenticationProvider();
+		SunJaasKerberosClient client = new SunJaasKerberosClient();
+		client.setDebug(true);
+		provider.setKerberosClient(client);
+		provider.setUserDetailsService(dummyUserDetailsService());
+		return provider;
+	}
+
+	@Bean
+	public KerberosServiceAuthenticationProvider kerberosServiceAuthenticationProvider() {
+		KerberosServiceAuthenticationProvider provider = new KerberosServiceAuthenticationProvider();
+		provider.setTicketValidator(sunJaasKerberosTicketValidator());
+      provider.setUserDetailsService(dummyUserDetailsService());
+		return provider;
+	}
+
+	@Bean
+	public SunJaasKerberosTicketValidator sunJaasKerberosTicketValidator() {
+		SunJaasKerberosTicketValidator ticketValidator = new SunJaasKerberosTicketValidator();
+		ticketValidator.setServicePrincipal("HTTP/localhost");
+		ticketValidator.setKeyTabLocation(new FileSystemResource("/home/rainersawitzki/tomcat_service.keytab"));
+		ticketValidator.setDebug(true);
+		return ticketValidator;
+	}
+
+    @Bean
+    public DummyUserDetailsService dummyUserDetailsService() {
+      return new DummyUserDetailsService();
+    }
+    
+    public class DummyUserDetailsService implements UserDetailsService {
+
+    	  @Override
+    	  public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+    	    System.out.println(s);
+    	    return new User(s, "notUsed", true, true, true, true, AuthorityUtils.createAuthorityList("ROLE_USER"));
+    	  }
+    	}
 }
