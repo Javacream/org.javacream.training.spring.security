ad49c0e6edfb3b2e9c3aba83c88ae60c81b65d08
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-11-10 14:16:03 +0100

    add Authentication and Auzorization

---
+++ org.javacream.training.security.webservice.jar/src/main/java/org/javacream/training/security/webservice/WebSecurityConfiguration.java
@@ -0,0 +1,32 @@
+package org.javacream.training.security.webservice;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@SuppressWarnings("deprecation")
+	@Override
+	@Bean
+	protected UserDetailsService userDetailsService() {
+		UserDetails user = User.withDefaultPasswordEncoder().username("user").password("password").roles("user")
+				.build();
+		return new InMemoryUserDetailsManager(user);
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/api/anonymous").permitAll().anyRequest().authenticated().and()
+				.formLogin().permitAll().and().logout().permitAll();
+	}
+
+}
